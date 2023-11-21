340a51d4ab552782b038aa49caab9c15cee49979
HeinMinLatt1235 <123471377+HeinMinLatt1235@users.noreply.github.com>
2023-11-21 10:14:26 +0630

    add customer controller and routes

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const customerRoutes = require('./customerRoutes')
 
 router.get("/user", () => console.log("using user"));
+router.use('/customer',customerRoutes);
 
 module.exports = router;
