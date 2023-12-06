fd2b3c8c5a87900089b1af229c2155c09ae41e3a
HponeMyatThu <hponemy8tthu@gmail.com>
2023-12-06 14:50:49 +0630

    [ bug ] fix bug eventController

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -290,14 +290,6 @@ const getEventsByOrganizerId = async (req, res) => {
   }
   return res.json(events);
 };
-const getEventsByOrganizer_Id = async (req, res) => {
-  const {organizerId} = req.params;
-  const events = await eventService.get_event_by_organizer_id(organizerId);
-  if (events.error) {
-    return res.status(404).json("No Data Found");
-  }
-  return res.json(events);
-};
 
 const getEventsByOrganizer_Id = async (req, res) => {
   const { organizerId } = req.params;
